a8dc0ab22bef7018ddbdff6138a7be5161f1ee3c
Pablwo Araújo <44266103+pablwoAraujo@users.noreply.github.com>
2023-03-30 16:46:30 -0300

    aula 04 - desafio 03

--- src/main/java/br/com/alura/programming/languages/api/LanguagesController.java
+++ src/main/java/br/com/alura/programming/languages/api/LanguagesController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -42,8 +43,9 @@ public class LanguagesController {
 	}
 
 	@PostMapping("/languages")
-	public Language createLanguage(@RequestBody Language language) {
-		return languageRepository.save(language);
+	public ResponseEntity<Language> createLanguage(@RequestBody Language language) {
+		Language savedLanguage = languageRepository.save(language);
+		return new ResponseEntity<>(savedLanguage, HttpStatus.CREATED);
 	}
 
 	@PutMapping("/languages/{id}")
