a56eac0ca9a180ee332081480f9932534c8b9611
Pablwo Araújo <44266103+pablwoAraujo@users.noreply.github.com>
2023-03-30 16:33:51 -0300

    aula 04 - desafio 01

--- src/main/java/br/com/alura/programming/languages/api/Language.java
+++ src/main/java/br/com/alura/programming/languages/api/Language.java
@@ -26,6 +26,10 @@ public class Language {
 		return id;
 	}
 
+	public void setId(String id) {
+		this.id = id;
+	}
+
 	public String getName() {
 		return name;
 	}
--- src/main/java/br/com/alura/programming/languages/api/LanguagesController.java
+++ src/main/java/br/com/alura/programming/languages/api/LanguagesController.java
@@ -1,12 +1,19 @@
 package br.com.alura.programming.languages.api;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 public class LanguagesController {
@@ -25,8 +32,32 @@ public class LanguagesController {
 		return languages;
 	}
 
+	@GetMapping("/languages/{id}")
+	public Language getLanguageById(@PathVariable String id) {
+		return languageRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+	}
+
 	@PostMapping("/languages")
 	public Language createLanguage(@RequestBody Language language) {
 		return languageRepository.save(language);
+	}
+
+	@PutMapping("/languages/{id}")
+	public Language updateLanguage(@RequestBody Language language, @PathVariable String id) {
+		// languageRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+		if (!languageRepository.existsById(id)) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+		}
+		language.setId(id);
+		Language savedLanguage = languageRepository.save(language);
+		return savedLanguage;
+	}
+
+	@DeleteMapping("/languages/{id}")
+	public void deleteLanguage(@PathVariable String id) {
+		if (!languageRepository.existsById(id)) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+		}
+		languageRepository.deleteById(id);
 	}
 }
